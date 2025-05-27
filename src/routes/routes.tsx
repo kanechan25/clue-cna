700cd59c9b7ca67e57f8b8765262decda2788bcb
kane <85122363+kanechan25@users.noreply.github.com>
2025-05-27 13:24:01 +0700

    Initial commit

---
+++ src/routes/routes.tsx
@@ -0,0 +1,20 @@
+import Example from '@/components/Example'
+import { Routes } from '@/models/types'
+import Home from '@/pages/Home'
+
+export const routers: Routes[] = [
+  {
+    href: '/',
+    id: 'home',
+    name: 'Home',
+    element: <Home />,
+  },
+  {
+    href: '/example',
+    id: 'example',
+    name: 'Example',
+    element: <Example />,
+  },
+
+  // Add other routes as needed
+]
